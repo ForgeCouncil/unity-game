e62dc39f734648bb7f183afacac534c761b35407
Ian <ian.lehine@gmail.com>
2020-06-29 01:01:04 -0700

    Added small script that makes .png sprites alwas face objects tagged as Player. Re-added skeleton for potion management script, example in Sandbox scene (spawns a tree instead of particle fx, makes player 2x bigger.)

---
+++ Assets/FacePlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacePlayer : MonoBehaviour
+{
+    public GameObject player;
+    
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void Update()
+    {
+        this.transform.LookAt(player.transform);
+    }
+}
---
+++ Assets/Scripts/Potion.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potion : MonoBehaviour
+{
+    //multiplier
+    public float potency = 5;
+
+    //particles
+    public GameObject particles;
+
+    //Check for collision with potion
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pickup(other);
+        }
+    }
+
+    void Pickup(Collider player)
+    {
+        // call particles
+        Instantiate(particles, transform.position, transform.rotation);
+        
+        //size
+        player.transform.localScale *= potency;
+        
+        //remove potion from screen/inventory
+        Destroy(gameObject);
+    }
+}
