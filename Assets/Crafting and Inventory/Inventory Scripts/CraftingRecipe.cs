53d844f41dc033c33eae6a43648e6c517ae9783f
Ian <ian.lehine@gmail.com>
2020-07-08 15:44:18 -0700

    Experiments with inventory/crafting

---
+++ Assets/Crafting and Inventory/Inventory Scripts/CraftingRecipe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    [Serializable]
+public struct ItemAmount
+{
+    public Item Item;
+    [Range(1, 999)]
+    public int Amount;
+}
+    [CreateAssetMenu]
+public class CraftingRecipe : ScriptableObject
+{
+    public List<ItemAmount> Materials;
+    public List<ItemAmount> Result;
+
+}
---
+++ Assets/Crafting and Inventory/Inventory Scripts/MenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject inventoryMenu;
+    private bool inMenu = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            inventoryMenu.gameObject.SetActive(!inventoryMenu.activeSelf);
+            
+            //Freeze movements and free up cursor when menu is up
+            inMenu = !inMenu;
+            if (inMenu == true)
+                print("Menu is up, freeze controls and free up the cursor.");
+        }
+    }
+}
