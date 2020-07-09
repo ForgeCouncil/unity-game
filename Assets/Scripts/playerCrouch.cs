cc370daed9a25e776c64e1e902fd622b1e066a47
Ricky Pena <ricardopena737@yahoo.com>
2020-07-08 19:04:39 -0700

    basic crouch, to be improved upon.

---
+++ Assets/Scripts/playerCrouch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCrouch : MonoBehaviour
+{
+   CharacterController characterCollider;
+    void Start()
+    {
+        characterCollider = gameObject.GetComponent<CharacterController> ();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl)){
+            characterCollider.height = 1.9f;
+        }
+        else {
+            characterCollider.height = 3.8f;
+        }
+    }
+}
