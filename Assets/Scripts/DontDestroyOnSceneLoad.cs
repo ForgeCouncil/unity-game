dcb12065e7343a9f7fea5f8679cb1f57a8b66fa2
Ian <ian.lehine@gmail.com>
2020-09-26 15:54:43 +0200

    THERE CAN BE ONLY ONE!! Made player, UI, controller into a persistent singleton.

--- Assets/Scripts/DontDestroyOnSceneLoad.cs
+++ Assets/Scripts/DontDestroyOnSceneLoad.cs
@@ -4,9 +4,22 @@ using UnityEngine.SceneManagement;
 
 public class DontDestroyOnSceneLoad : MonoBehaviour
 {
+    //Making objects with this script attached into singletons.
+    static DontDestroyOnSceneLoad Single;
+
     void Start()
     {
+        if(Single != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        
+        //there can only be one
+        Single = this;
+        //become immortal 
         GameObject.DontDestroyOnLoad(this.gameObject);
+        
     }
 
     void OnDestroy()
