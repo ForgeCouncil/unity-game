ea94efd726ab9902f584215d616af241dd99c9ee
Ian <ian.lehine@gmail.com>
2020-09-26 16:23:47 +0200

    haha github go brrrr

--- Assets/InventoryImport/Scripts/SceneSwitch.cs
+++ Assets/InventoryImport/Scripts/SceneSwitch.cs
@@ -6,14 +6,35 @@ using UnityEngine.SceneManagement;
 public class SceneSwitch : MonoBehaviour
 {
     public Vector3 outLocation = new Vector3(100, 100, 100);
+    private int sceneID;
+    public enum SceneTarget
+    {
+        StartingArea, Duncan
+    }
+    public SceneTarget portalTarget;
+
+    void Start()
+    {
+        if(portalTarget == SceneTarget.Duncan)
+        {
+            sceneID = 1;
+        }
+
+        if(portalTarget == SceneTarget.StartingArea)
+        {
+            sceneID = 0;
+        }
+    }
 
     void OnTriggerEnter(Collider player)
     {
-        player.transform.position = outLocation;
+        //$5 to anyone that can figure out why the player maintains its transform 
+        //when it goes through the portal even with the following line active. >_<
+        //player.transform.position = outLocation;
 
         if(player.tag == "Player")
         {
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(sceneID);
         }
     }
 }
