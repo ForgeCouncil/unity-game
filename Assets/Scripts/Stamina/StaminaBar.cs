c7b8090745016e3c27c1a35463f26ef0063d6362
Shaky <bryce.sturgis2759@gmail.com>
2020-09-26 10:14:24 -0500

    Gradient Fixes

--- Assets/Scripts/Stamina/StaminaBar.cs
+++ Assets/Scripts/Stamina/StaminaBar.cs
@@ -5,28 +5,28 @@ using UnityEngine.UI; // used to interact with the health bar (graphic)
 
 public class StaminaBar : MonoBehaviour
 {
-    public Slider slider; 
-    public Gradient gradient;
-    public Image fill;
+    public Slider slider2; 
+    public Gradient gradient2;
+    public Image fill2;
     private Vector3 scaleChange, positionChange;
     
 
     void start() {
-        slider.value = 100; //sets the health bar to that value
+        slider2.value = 100; //sets the health bar to that value
     }
 
     public void SetMaxStamina(int stamina)
     {
-        slider.maxValue = stamina; //sets health bar's max to maxHealth 
+        slider2.maxValue = stamina; //sets health bar's max to maxHealth 
         
-        fill.color = gradient.Evaluate(1f); //allows the health bar to go from green to yellow to red
+        fill2.color = gradient2.Evaluate(1f); //allows the health bar to go from green to yellow to red
     }
 
     public void SetStamina(int stamina)
     {
-        slider.value = stamina; //changes health bar's value to new health value
+        slider2.value = stamina; //changes health bar's value to new health value
 
-        fill.color = gradient.Evaluate(slider.normalizedValue); // updates the color of the health bar
+        fill2.color = gradient2.Evaluate(slider2.normalizedValue); // updates the color of the health bar
     }
 
 }
