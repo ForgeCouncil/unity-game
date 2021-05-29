37f3b67b52864de8867faeaee8c982528e3a93b1
Algirdukan <Algird_minion@hotmail.com>
2021-05-29 10:55:10 -0700

    Floating Islands Update, Trial 1 complete

---
+++ Assets/Scripts/World Scripts/Rotata.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using UnityEngine;
+using System.Collections;
+
+ public class Rotata : MonoBehaviour
+{
+    public Transform target;
+
+
+    void Update()
+    {
+        Vector3 relativePos = (target.position + new Vector3(0, 5f, 0)) - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+
+        Quaternion current = transform.localRotation;
+
+        transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime);
+        transform.Translate(0, 0, 5 * Time.deltaTime);
+    }
+}
